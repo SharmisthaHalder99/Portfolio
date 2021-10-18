bc42ebb49bc1b63132502f36a4e32a586ce42782
SharmisthaHalder99 <shalder99.ee@gmail.com>
2021-10-18 19:48:04 +0530

    light_theme

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector(".menu")
 const navEle = document.querySelector(".menu_elements")
 const img = document.querySelector("#menu_img")
+const dark_btn = document.querySelector(".dark_btn")
+const light_btn = document.querySelector(".light_btn")
+
 menu.addEventListener("click",e=>{
     if(navEle.style.display=="flex" ){
         return navEle.style.display="none"
@@ -20,4 +23,38 @@ window.addEventListener("click",e =>{
     }
         }
 
+})
+light_btn.addEventListener("click", e=>{
+    const body =document.querySelector("body")
+    body.classList.toggle("light_body")
+    
+    const hero = document.querySelector(".hero-ele")
+    hero.classList.toggle("light_hero")
+
+    const nav = document.querySelector("nav")
+    nav.classList.toggle("light_nav")
+
+    const project = document.querySelector(".project-ele")
+    project.classList.toggle("light_project")
+
+    const about = document.querySelector(".about_me")
+    about.classList.toggle("light_about")
+
+    const skill = document.querySelector(".skills")
+    skill.classList.toggle("light_skill")
+
+    const education = document.querySelector(".education")
+    education.classList.toggle("light_education")
+
+    const contact = document.querySelector(".contact")
+    contact.classList.toggle("light_contact")
+
+    const footer = document.querySelector("footer")
+    footer.classList.toggle("light_footer")
+
+    const copy = document.querySelector(".copyright")
+    copy.classList.toggle("light_copyright")
+
+    const toggle = document.querySelector(".toggle")
+    toggle.classList.toggle("light_toggle")
 })
