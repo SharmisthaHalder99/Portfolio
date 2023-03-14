d6505977ea8f46e9ba687ccfd3a582a747dd38ea
SharmisthaHalder99 <shalder99.ee@gmail.com>
2023-03-14 20:46:40 +0530

    new portfolio added

--- project.js
+++
@@ -1,7 +0,0 @@
-if(localStorage.getItem('theme')=='light'){
-    document.body.style.backgroundColor = "#E0DAE9" 
-    const dark = document.querySelectorAll(".text")
-    for(let i = 0;i<dark.length;i++){
-        dark[i].classList.toggle("dark_text")
-    }
-}
